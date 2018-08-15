98857bf011b0f6049ef2b3109774fd39c709853f
Karl Lv <karl@fedora24.epam.com>
2018-08-15 15:14:41 +0800

    nop

--- example_8.sh
+++ example_8.sh
@@ -7,20 +7,20 @@
 file=/dir1/dir2/dir3/my.file.txt
 
 echo $file
-echo ${file#*/}
-echo ${file##*/}
-echo ${file#*.}
-echo ${file##*.}
-echo ${file%/*}
-echo ${file%%/*}
-echo ${file%.*}
-echo ${file%%.*}
+echo ${file#*/}         # dir1/dir2/dir3/my.file.txt
+echo ${file##*/}        # my.file.txt
+echo ${file#*.}         # file.txt
+echo ${file##*.}        # txt
+echo ${file%/*}         # /dir1/dir2/dir3
+echo ${file%%/*}        # null output
+echo ${file%.*}         # /dir1/dir2/dir3/my.file
+echo ${file%%.*}        # /dir1/dir2/dir3/my
 
 echo
-echo ${file:0:5}
-echo ${file:5:5}
+echo ${file:0:5}        # /dir1
+echo ${file:5:5}        # /dir2
 echo
-echo ${file/dir/path}
-echo ${file//dir/path}
+echo ${file/dir/path}   # /path1/dir2/dir3/my.file.txt
+echo ${file//dir/path}  # /path1/path2/path3/my.file.txt
 echo
-echo ${#file}
+echo ${#file}           # 27
